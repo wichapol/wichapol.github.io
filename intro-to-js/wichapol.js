10769dfe0dcc3ec65e6de7f2d130e9826703d3f6
wichapol jaimaung <wichapol.44424@gmail.com>
2022-02-18 14:34:04 +0700

    Intro js 2

--- intro-to-js/wichapol.js
+++ intro-to-js/wichapol.js
@@ -10,3 +10,21 @@ function helloWorld() {
     const h1message = document.getElementById("what-my-name").innerHTML 
        alert(h1message + '  งดแปลว่ายังทำได้อยู่')
     }
+
+function calAge() {
+    const YearBorn = prompt("what year were you born");
+    const YearNow  = new Date().getFullYear();    
+    let Age = YearNow-YearBorn; 
+    const message = "ยินดีคุณทนอยู่ในประเทศนี้มา" 
+    const message2 = "ปีแล้ว"
+    alert(`${message} ${Age} ${message2}`);
+}
+
+
+function loadProfile() {
+    const name2 = wichapol
+    const province = jaimaung
+
+    document.getElementById("my-name").innerHTML = name2;
+    document.getElementById("my-province").innerHTML = province;
+}
